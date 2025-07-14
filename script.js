1e9ad6ade6dff2a4de5e3d4b65fc2936480b96f2
SalvadorTorre <saltor03@hotmail.com>
2025-07-14 07:15:02 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 // Aquí puedes agregar funciones JavaScript personalizadas más adelante
+let slideIndex = 0;
+const slides = document.querySelectorAll(".slide");
+
+function showSlides() {
+  slides.forEach(slide => slide.classList.remove("active"));
+  slideIndex = (slideIndex + 1) % slides.length;
+  slides[slideIndex].classList.add("active");
+}
+
+setInterval(showSlides, 3000); // Cambia cada 3 segundos
